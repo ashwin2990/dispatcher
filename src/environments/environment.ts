e8ff20b839809fe7fe74c65543e9161075f65eb3
ashwin2990 <ashwin29@gmail.com>
2020-04-17 00:46:52 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,13 +15,5 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBr8RVx3ABY2TIjoKKv7bDxDtZuG_X3keo",
-    authDomain: "subscriptiondispatcherapp.firebaseapp.com",
-    databaseURL: "https://subscriptiondispatcherapp.firebaseio.com",
-    projectId: "subscriptiondispatcherapp",
-    storageBucket: "subscriptiondispatcherapp.appspot.com",
-    messagingSenderId: "635564565647",
-    appId: "1:635564565647:web:ee6e124c7d59908014fcfa",
-    measurementId: "G-XS9T9XPVQN"
   },
 };
